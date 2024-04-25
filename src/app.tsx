d5bc7700ef92df58666133f1535a5557140d1bfa
Riskirizki <85600599+Riskirizki@users.noreply.github.com>
2024-04-25 19:36:10 +0700

    chore: add tailwind css, postcss, and autoprefixer as development dependencies

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,9 @@ import { LearningList } from "./components/learning-list";
 export function App() {
   return (
     <div>
-      <h1>Welcome to Learnify Study</h1>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to Learnify Study!
+      </h1>
       <LearningList />
     </div>
   );
