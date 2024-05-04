ec49dffd7e457ab522ceda4438cb34e1354c2b59
Riskirizki <85600599+Riskirizki@users.noreply.github.com>
2024-05-04 15:32:24 +0700

    chore: enhanced the display and interaction on the learning detail page.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { App } from "./app";
-import { Detail } from "./pages/detail";
+import { Detail } from "./pages/detail.tsx";
 import "./index.css";
 
 const router = createBrowserRouter([
